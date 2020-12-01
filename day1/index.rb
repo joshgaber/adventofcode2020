5fe69ccf81149c315f0b08d5b3c359d4ab1f4af9
Josh Gaber <j.gaber@vehikl.com>
2020-12-01 08:50:44 -0500

    Day 1 complete

--- day1/index.rb
+++ day1/index.rb
@@ -1,4 +1,23 @@
 class Day1
-  def self.part1
+  def initialize
+    @entries = File.read('./day1/input.txt').strip.split("\n").map(&:to_i)
+  end
+
+  def part1
+    smaller = @entries.filter { |entry| entry < 1010 }
+    num1 = smaller.filter { |entry| @entries.include?(2020 - entry)}.first
+    num2 = 2020 - num1
+
+    "Product of both entries: #{num1 * num2}"
+  end
+
+  def part2
+    smaller = @entries.filter { |entry| entry < 1010 }
+    grid = smaller.map { |entry| smaller.map { |sm| sm + entry } }.flatten
+    num1 = @entries.filter { |entry| grid.include?(2020 - entry)}.first
+    num2 = smaller.filter{ |entry| smaller.include?(2020 - num1 - entry)}.first
+    num3 = 2020 - num1 - num2
+
+    "Product of all three entries: #{num1 * num2 * num3}"
   end
 end
--- index.rb
+++ index.rb
@@ -3,8 +3,13 @@
 end
 
 day = (ARGV[0] || 1).to_i
-part = (ARGV[1] || 1).to_i
 
-puts "Day #{day}: Part #{part}"
-
-Object.const_get("Day#{day}").send("part#{part}")
+if (ARGV[1])
+  puts "== Day #{day}: Part #{ARGV[1]} =="
+  puts Object.const_get("Day#{day}").new.send("part#{ARGV[1]}")
+else
+  puts "== DAY #{day} =="
+  ref = Object.const_get("Day#{day}").new
+  puts ref.part1
+  puts ref.part2
+end
