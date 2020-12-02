81804b34b1e79ad83579d53bdb849b62cdcd017f
Josh Gaber <j.gaber@vehikl.com>
2020-12-02 09:08:04 -0500

    Day 2 complete

--- day2/index.rb
+++ day2/index.rb
@@ -1,11 +1,15 @@
 class Day2
   def initialize
+    @passwords = File.read('./day2/input.txt').strip.split("\n").map { |p| /(\d+)-(\d+) (\w): (\w+)/.match(p).to_a.slice(1...5) }
   end
 
   def part1
+    good = @passwords.filter { |p| (p[0].to_i...p[1].to_i+1).include?(p[3].scan(Regexp.new(p[2])).count) }
+    "Good passwords: #{good.count}"
   end
 
   def part2
+    good = @passwords.filter { |p| (p[3][p[0].to_i-1] == p[2]) ^ (p[3][p[1].to_i-1] == p[2]) }
+    "Actually good passwords: #{good.count}"
   end
 end
-
