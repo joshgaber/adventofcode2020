9b5b3c3a8210a33f6f04751c426b14587f1efd0b
Josh Gaber <j.gaber@vehikl.com>
2020-12-05 09:16:04 -0500

    Day 5 complete

--- day5/index.rb
+++ day5/index.rb
@@ -1,11 +1,13 @@
 class Day5
   def initialize
+    @seats = File.read('day5/input.txt').strip.split("\n").map { |s| s.gsub(/R|B/, "1").gsub(/L|F/, "0").to_i 2 }
   end
 
   def part1
+    "Highest seat: #{@seats.max}"
   end
 
   def part2
+    "Your seat: #{(@seats.min..@seats.max).to_a.difference(@seats).first}"
   end
 end
-
