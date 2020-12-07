cee69897882fbe9e4e8f4c798d288d6c13619f8f
Josh Gaber <j.gaber@vehikl.com>
2020-12-07 10:06:11 -0500

    Day 7 complete

--- day7/index.rb
+++ day7/index.rb
@@ -1,11 +1,24 @@
 class Day7
   def initialize
+    @bags = File.read('day7/input.txt').gsub(/\s*bags?|\./, "").split("\n").to_h { |b| b.split /\s*contain\s*/}
   end
 
   def part1
+    bags = @bags.transform_values { |b| b.gsub(/\d+\s*/, "").split /,\s*/ }
+    "Bags that contain shiny gold: #{bags.select { |b| hasShinyGoldBag? b,bags }.length}"
   end
 
   def part2
+    bags = @bags.transform_values { |b| b.split(/,\s*/).map { |bb| bb.split(/\s+/, 2) } }
+    "Bags in a shiny gold: #{containsBag("shiny gold", bags)}"
+  end
+
+  def hasShinyGoldBag?(bag, bags)
+    return true if bags[bag].to_a.include?("shiny gold")
+    bags[bag].to_a.reduce(false) { |acc, b| acc || hasShinyGoldBag?(b,bags) }
+  end
+
+  def containsBag(bag, bags)
+    bags[bag].to_a.reduce(0) { |acc, b| acc + (1 + containsBag(b[1],bags)) * b[0].to_i }
   end
 end
-
