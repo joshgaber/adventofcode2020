be8ff02a009eb9c0a0dfa1c7ba3f9ec3354b5440
Josh Gaber <j.gaber@vehikl.com>
2020-12-11 18:05:08 -0500

    Day 11 Complete

--- day11/index.rb
+++ day11/index.rb
@@ -1,70 +1,62 @@
 class Day11
   def initialize
-    @setup = File.read('day11/input.txt').strip.split("\n").map &:chars
+    @seats = File.read('day11/input.txt').strip.split("\n").map &:chars
   end
 
   def part1
-    setup = @setup.map.with_index do |s, i|
-      s.map.with_index do |v, j|
-        {
-          val: v,
-          next: ([i-1, 0].max..[i+1, @setup.length-1].min).to_a.product(([j-1, 0].max..[j+1, @setup[0].length-1].min).to_a) - [[i, j]]
-        }
-      end
-    end
-    # return setup[0][0].inspect
-    # changes = Array.new(setup.length) {Array.new(setup[0].length)}
-    # return changes.flatten.count
-    loop do
-      changes = setup.map { |s| s.clone }
-      for r in (0...setup.length)
-        for c in (0...setup[r].length)
-          changes[r][c][:val] = checkAdjacent setup, r, c
-        end
-      end
-      return changes.flatten.map { |s| s[:val] }.count("#") if setup.flatten.map { |s| s[:val] }.join == changes.flatten.map { |s| s[:val] }.join
-      setup = changes
-    end
+    seats = @seats.map.with_index { |s, r| s.map.with_index { |val, c|
+      {
+        val: val,
+        next: ([r-1, 0].max..[r+1, @seats.length-1].min).to_a.product(([c-1, 0].max..[c+1, @seats[0].length-1].min).to_a) - [[r, c]]
+      }
+    } }
+    "Adjacent seats: #{process seats, 4}"
   end
 
   def part2
-    # return 'not done'
-    setup = @setup
+    seats = @seats.map.with_index { |s, r| s.map.with_index { |val, c|
+      {
+        val: val,
+        next: visibleSeats(r, c)
+      }
+    } }
+    "Adjacent seats: #{process seats, 5}"
+  end
+
+  def visibleSeats(row, column)
+    return [] if @seats[row][column] == '.'
+    ([-1, 0, 1].product([-1, 0, 1]) - [[0, 0]]).map { |d| findNextVisible row, column, *d }.compact
+  end
+
+  def findNextVisible(row, column, v, h)
     loop do
-      changes = []
-      for r in (0...setup.length)
-        changes[r] = []
-        for c in (0...setup[r].length)
-          changes[r][c] = checkVisible setup, r, c
+      row += v
+      column += h
+      return nil unless (0...@seats.length).include?(row) && (0...@seats[0].length).include?(column)
+      return [row, column] if @seats[row][column] == 'L'
+    end
+  end
+
+  def process(setup, tolerance)
+    loop do
+      changes = setup.map { |s| s.map { |t| t.clone } }
+      setup.each_index do |r|
+        setup[r].each_index do |c|
+          changes[r][c][:val] = lookAround setup, r, c, tolerance
         end
       end
-      return changes.flatten.count("#") if setup.to_s == changes.to_s
+      return changes.flatten.count { |s| s[:val] == '#' } if setup.flatten.map { |s| s[:val] }.join == changes.flatten.map { |s| s[:val] }.join
       setup = changes
     end
   end
 
-  def checkAdjacent(setup, r, c)
+  def lookAround(setup, r, c, tolerance)
     return '.' if setup[r][c][:val] == '.'
-    # adjacentRow = ([r-1, 0].max..[r+1, setup.length].min)
-    # adjacentColumn = ([c-1, 0].max..[c+1, setup[0].length].min)
-    # puts setup[r][c].inspect
-    adjacent = setup[r][c][:next].map { |n| setup[n[0]][n[1]][:val] }
+    occupied = setup[r][c][:next].count { |n| setup.dig(*n)[:val] == '#' }
     if (setup[r][c][:val] == 'L')
-      adjacent.count('#') == 0 ? '#' : 'L'
+      occupied == 0 ? '#' : 'L'
     else
-      adjacent.count('#') <= 4 ? '#' : 'L'
-    end
-  end
-
-  def checkVisible(setup, r, c)
-    return '.' if setup[r][c] == '.'
-    adjacentRow = ([r-1, 0].max..[r+1, setup.length].min)
-    adjacentColumn = ([c-1, 0].max..[c+1, setup[0].length].min)
-    adjacent = setup[adjacentRow].map { |s| s[adjacentColumn] }.flatten
-    if (setup[r][c] == 'L')
-      adjacent.count('#') == 0 ? '#' : 'L'
-    else
-      adjacent.count('#') <= 4 ? '#' : 'L'
+      occupied < tolerance ? '#' : 'L'
     end
   end
 end
