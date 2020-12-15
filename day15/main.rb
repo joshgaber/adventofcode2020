e7d0d0727220544967b60e1a295c06d9e022f7ff
Josh Gaber <j.gaber@vehikl.com>
2020-12-15 00:43:27 -0500

    Day 15 complete

--- day15/main.rb
+++ day15/main.rb
@@ -1,12 +1,22 @@
 module Day15
   class Main
-    def initialize
-    end
-
     def part1
+      "2020th number: #{process 2020}"
     end
 
     def part2
+      "30000000th number: #{process 30000000}"
+    end
+
+    def process(nth)
+      list = {12 => 0, 20 => 1, 0 => 2, 6 => 3, 1 => 4, 17 => 5, 7 => 6}
+      last = 0
+      (7...nth - 1).each do |i|
+        newLast = (list.has_key?(last) ? i - list[last] : 0)
+        list[last] = i
+        last = newLast
+      end
+      last
     end
   end
 end
