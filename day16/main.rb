c0f7df747dbd474d827e565ae7ad0cb4aa96e46b
Josh Gaber <j.gaber@vehikl.com>
2020-12-16 00:27:36 -0500

    Day 16 part 1 complete

--- day16/main.rb
+++ day16/main.rb
@@ -1,12 +1,27 @@
 module Day16
   class Main
     def initialize
+      input = File.read("day16/input.txt").strip.split("\n\n")
+      @fields = input[0].split("\n").to_h { |field| field.split /:\s*/, 2 }
+      @tickets = input[2].split("\n")[1..-1].map { |i| i.split "," }
     end
 
     def part1
+      fields = @fields.values.map { |field| field.split(/\s*or\s*/).map { |i| Range.new(*i.split("-")).to_a }.flatten }.reduce(:|)
+      tickets = @tickets.reduce(:+)
+      invalidNumbers.map(&:to_i).sum
     end
 
     def part2
+      invalid = invalidNumbers
+      validTickets = @tickets.select { |ticket| (ticket & invalidNumbers).empty? }
+      validTickets.count
+    end
+
+    def invalidNumbers
+      fields = @fields.values.map { |field| field.split(/\s*or\s*/).map { |i| Range.new(*i.split("-")).to_a }.flatten }.reduce(:|)
+      tickets = @tickets.reduce(:+)
+      tickets - fields
     end
   end
 end
