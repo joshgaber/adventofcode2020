42e5173cd76e2c294afeb445eebc045b5c23d325
Josh Gaber <j.gaber@vehikl.com>
2020-12-16 09:27:56 -0500

    Day 16 Complete

--- day16/main.rb
+++ day16/main.rb
@@ -2,26 +2,47 @@ module Day16
   class Main
     def initialize
       input = File.read("day16/input.txt").strip.split("\n\n")
-      @fields = input[0].split("\n").to_h { |field| field.split /:\s*/, 2 }
+      @fields = input[0].split("\n").to_h { |field| field.split /:\s*/, 2 }.transform_values do |field|
+        field.split(/\s*or\s*/).map { |i| Range.new(*i.split("-")).to_a }.flatten
+      end
+      @myTicket = input[1].split("\n")[1].split ","
       @tickets = input[2].split("\n")[1..-1].map { |i| i.split "," }
     end
 
     def part1
-      fields = @fields.values.map { |field| field.split(/\s*or\s*/).map { |i| Range.new(*i.split("-")).to_a }.flatten }.reduce(:|)
-      tickets = @tickets.reduce(:+)
-      invalidNumbers.map(&:to_i).sum
+      "Error rate: #{invalidNumbers.map(&:to_i).sum}"
     end
 
     def part2
-      invalid = invalidNumbers
-      validTickets = @tickets.select { |ticket| (ticket & invalidNumbers).empty? }
-      validTickets.count
+      invalid = invalidNumbers.uniq
+      validTickets = @tickets.select { |ticket| (ticket & invalid).empty? }
+
+      possibleFields = validTickets.transpose.map do |column|
+        @fields.select { |k, v| (column & v).count == column.uniq.count }.keys
+      end
+
+      departureProduct = mapFieldToIndex(possibleFields).select do |k, v|
+        k.include? "departure"
+      end.values.map { |i| @myTicket[i].to_i }.reduce :*
+
+      "Product of departure fields: #{departureProduct}"
     end
 
     def invalidNumbers
-      fields = @fields.values.map { |field| field.split(/\s*or\s*/).map { |i| Range.new(*i.split("-")).to_a }.flatten }.reduce(:|)
-      tickets = @tickets.reduce(:+)
-      tickets - fields
+      @tickets.reduce(:+) - @fields.values.reduce(:|)
+    end
+
+    def mapFieldToIndex(matches)
+      fieldMap = {}
+      loop do
+        matches.each.with_index do |m, i|
+          if m.count == 1
+            fieldMap[fm.first] = i
+            return fieldMap if fieldMap.size == @fields.size
+            matches.map! { |mm| mm - m }
+          end
+        end
+      end
     end
   end
 end
