9fb9b84d54ee6aea8139e67b4f456b7b2413412d
Josh Gaber <j.gaber@vehikl.com>
2020-12-18 00:38:15 -0500

    Day 18 complete

--- day18/main.rb
+++ day18/main.rb
@@ -1,12 +1,17 @@
+require_relative 'problem'
+
 module Day18
   class Main
     def initialize
+      @problems = File.read('day18/input.txt').strip.split("\n").map { |p| Problem.new p }
     end
 
     def part1
+      "With equal precedent: #{@problems.map { |p| p.solve ['+*'] }.sum}"
     end
 
     def part2
+      "With addition precedent: #{@problems.map { |p| p.solve ['+', '*'] }.sum}"
     end
   end
 end
---
+++ day18/problem.rb
@@ -0,0 +1,22 @@
+module Day18
+  class Problem
+    def initialize(problem)
+      @problem = problem
+    end
+
+    def solve(ooo, problem = @problem)
+      problem = problem.sub(/\([^(]*?\)/) { |s| solve ooo, s[1..-2] } while problem.match?(/\(/)
+      values = problem.split(/\s+/)
+      ooo.each { |order| calculate values, order }
+      values.first
+    end
+
+    def calculate(values, operands)
+      loop do
+        i = values.index { |v| operands.include? v.to_s }
+        break if i === nil
+        values[i-1..i+1] = eval values[i-1..i+1].join
+      end
+    end
+  end
+end
