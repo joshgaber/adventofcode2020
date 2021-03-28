89d04b401d662fe1269c40a6a92aa8730bc8c971
Josh Gaber <j.gaber@vehikl.com>
2021-03-28 11:56:11 -0400

    Add documentation

--- main.rb
+++ main.rb
@@ -7,9 +7,11 @@ loop do
   day = gets.to_i
   break unless day.between? 1, 25
 
+  puts
   puts "== DAY #{day} =="
-  ref = Object.const_get("Day#{day}")::Main.new
-  puts ref.part1
-  puts ref.part2
+  ref = Object.const_get("Day#{day}")::Main.new(File.read("day#{day}/input.txt"))
+  puts "PART 1: #{ref.part1}"
+  puts "PART 2: #{ref.part2}"
   puts '== END =='
+  puts
 end
